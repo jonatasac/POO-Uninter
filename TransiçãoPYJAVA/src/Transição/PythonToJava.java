1ece39444d1c2a55fed3f7e0947434600570812a
Jonatas Canuto <jonatas.ac@hotmail.com>
2023-06-25 12:39:31 -0300

    Update POO

--- TransiçãoPYJAVA/src/Transição/PythonToJava.java
+++ TransiçãoPYJAVA/src/Transição/PythonToJava.java
@@ -1,6 +1,18 @@
 package Transição;
 
+import java.util.Scanner;
+
 public class PythonToJava {
+	
+	public static void print(float x) {
+		System.out.println(x);
+	}
+	
+	public static float media(float a, float b, float c) {
+		float resultado = (a+b+c)/3;
+		return resultado;
+	}
+	
 	public static void main(String[] args) {
 		/*System.out.println("Alo mamae");
 		System.out.println("Alo papai");
@@ -13,9 +25,41 @@ public class PythonToJava {
 		int tamanhoString = nome.length();
 		System.out.println("O tamanho da String eh: " + tamanhoString);*/
 		
-		int resultado;
+		/*int resultado;
 		resultado = 4 + 3;
 		System.out.println(resultado);
+		double resultado1;
+		resultado1 = 4d / 3;
+		System.out.println(resultado1);*/
+		
+		/*double resultado;
+		resultado = Math.pow(2,3); //numero elevado a potencia
+		resultado = Math.sqrt(9); //raiz quadrada
+		System.out.println(resultado);*/
+		
+		/*System.out.println("Digite sua idade:");
+		Scanner teclado = new Scanner(System.in); //Lê o teclado como entrada.
+		int idade = teclado.nextInt(); //Lê valor inteiro
+		System.out.println("Sua idade eh: " + idade + " anos!");
+		if(idade < 18) {
+			System.out.println("Voce eh Menor de idade!");
+		}
+		else if (idade < 65) {
+			System.out.println("Voce eh Adulto!");
+		}
+		else {
+			System.out.println("Voce ja eh um veiaco!");
+		}*/
+		
+		Scanner teclado = new Scanner(System.in);
+		/*String nome;
+		print("ola mundo, agora sim digite um nome");
+		nome = teclado.next();
+		print("Seu nome eh: " + nome);*/
+		float x = teclado.nextFloat();
+		float y = teclado.nextFloat();
+		float z = teclado.nextFloat();
+		print(media(x,y,z));
 		
 	}
 }
